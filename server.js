f90355549bacd2ee4ed898e213d0282d18f9855d
Julia <93089979+juverse@users.noreply.github.com>
2025-07-14 14:39:31 +0200

    fix api key

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const API_KEY = process.env.OPENROUTER_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 app.post("/chat", async (req, res) => {
   try {
